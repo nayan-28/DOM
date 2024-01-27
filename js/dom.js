35788eb546bf6d8d4b083024c8a624a4297caac4
nayan-28 <nayanmalakar720@gmail.com>
2024-01-27 13:06:58 +0600

    changing-innerHTML

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,6 @@
-const places = document.getElementsByClassName('important-name');
-console.log(places);
-
-for (const place of places) {
-    console.log(place.innerText);
-}
-
 const title = document.getElementById('places-name');
 title.setAttribute('title', 'Colo ghure asi');
+
+const fruits = document.getElementsByClassName('fruits-container');
+const changeDom = fruits[0].innerHTML = '<h2>Wow,its really Noise < /h2>'
+console.log(changeDom);
