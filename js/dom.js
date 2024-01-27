58a1c28c9958e2c9433744aa45956712707c7ebb
nayan-28 <nayanmalakar720@gmail.com>
2024-01-27 13:09:32 +0600

    innerText

--- js/dom.js
+++ js/dom.js
@@ -2,5 +2,5 @@ const title = document.getElementById('places-name');
 title.setAttribute('title', 'Colo ghure asi');
 
 const fruits = document.getElementsByClassName('fruits-container');
-const changeDom = fruits[0].innerHTML = '<h2>Wow,its really Noise < /h2>'
-console.log(changeDom);
+
+console.log(fruits[0].innerText);
