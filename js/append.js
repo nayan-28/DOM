4968d12e3190134f12e8528b9dd4a89cd68f07cd
nayan-28 <nayanmalakar720@gmail.com>
2024-01-27 15:13:56 +0600

    add sectiond using append

--- js/append.js
+++ js/append.js
@@ -3,3 +3,25 @@ const li = document.createElement('li');
 li.innerText = 'Rangamati';
 
 placesList.appendChild(li);
+
+const mainContainer = document.getElementById('main-content');
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My Food List'
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText = 'Biriyani'
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'Salad'
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'Mangso Kosha'
+ul.appendChild(li3);
+
+section.appendChild(ul);
+mainContainer.appendChild(section);
